27897c0c23785bafff173ea01fcc0bf1b993773c
xueyunlong <xueyunlong@cafintech.com>
2017-11-02 14:44:11 +0800

    测试配置中心

--- microservice-config-client/src/main/java/com/xyl/study/ConfigClientApplication.java
+++ microservice-config-client/src/main/java/com/xyl/study/ConfigClientApplication.java
@@ -1,7 +1,14 @@
 package com.xyl.study;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by xyl on 11/2/17.
  */
+@SpringBootApplication
 public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
 }
--- microservice-config-client/src/main/java/com/xyl/study/controller/ConfigClientController.java
+++ microservice-config-client/src/main/java/com/xyl/study/controller/ConfigClientController.java
@@ -1,7 +1,22 @@
 package com.xyl.study.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by xyl on 11/2/17.
+ * 这边的@RefreshScope注解不能少，否则即使调用/refresh，配置也不会刷新
  */
+@RestController
+@RefreshScope
 public class ConfigClientController {
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
+    }
 }
